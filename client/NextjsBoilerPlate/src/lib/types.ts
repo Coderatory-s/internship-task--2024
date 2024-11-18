2f36d157a184edfa228b2ce4d7c4b6769ece29df
FAAIZ AHMED <158812182+FaaizAhmed2004@users.noreply.github.com>
2024-11-18 11:16:44 +0500

    Delete client directory

--- client/NextjsBoilerPlate/src/lib/types.ts
+++
@@ -1,7 +0,0 @@
-export interface IDecryptedJwt {
-    id: string;
-    name: string;
-    email: string;
-    position: string; // 'HR', 'Employee', or any other role
-    status: string; // e.g., 'active', 'inactive', etc.
-  }
